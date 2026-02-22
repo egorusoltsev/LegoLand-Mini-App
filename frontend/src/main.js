c2f9c2822b4ad22a2ee705742b0bf3e917381e1a
egorusoltsev <egorusolcev498@gmail.com>
2026-02-22 21:24:35 +0300

    )

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,3 +1,18 @@
+window.addEventListener("error", (e) => {
+  document.body.innerHTML =
+    "<pre style='white-space:pre-wrap;padding:20px;font-size:14px;color:red'>" +
+    "JS ERROR:\n" + (e?.message || e) +
+    "</pre>"
+})
+
+window.addEventListener("unhandledrejection", (e) => {
+  document.body.innerHTML =
+    "<pre style='white-space:pre-wrap;padding:20px;font-size:14px;color:red'>" +
+    "PROMISE ERROR:\n" + (e?.reason || e) +
+    "</pre>"
+})
+
+
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
